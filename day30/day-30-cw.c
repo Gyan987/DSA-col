91c306311868a7fde895ad7797da0c1bcb1ab981
Gyan <gyanprakasht987@gmail.com>
2026-04-02 19:49:19 +0530

    add new update version of code

---
+++ day30/day-30-cw.c
@@ -0,0 +1,54 @@
+#include <stdio.h>
+int main()
+{
+    int a[100],i,choice,n=0;
+    int eent = 0;
+
+    do{
+        printf("1.Enter the size of the array:\n");
+        printf("2.Enter the array elementes:\n");
+        printf("3.Display\n");
+
+        printf("Enter your choice:");
+        scanf("%d",&choice);
+
+        switch(choice)
+        {
+            case 1:
+            printf("Enter the size of array:\n");
+            scanf("%d",&n);
+            eent=0;
+            break;
+
+            case 2:
+                if(n==0){
+                    printf("Enter the choice 1 first\n");
+                }
+                else{
+                    printf("Enter the array elements:\n");
+
+                    for(i=0;i<n;i++){
+                        scanf("%d",&a[i]);
+                    }
+                eent=1;
+            }
+            break;
+            case 3:
+                if(eent==0){
+                    printf("Enter the choice 1 and 2 first\n ");
+                }
+                else{
+                    printf("The array elements are :\n");
+                    choice++;
+                    for(i=0;i<n;i++){
+                        printf("%d ",a[i]);
+                    }
+                    printf("\n");
+                }
+                break;
+            default:
+               printf("Invalid Input");
+            }
+    }while(choice!=4);
+    return 0;
+}
